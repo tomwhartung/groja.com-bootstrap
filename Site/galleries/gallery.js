101888cc9e09dd40cbefc75111a62d315579f5b9
Tom W Hartung <tomwhartung@gmail.com>
2016-09-30 14:02:12 -0600

    Verified that we are getting the "images[0]" values and properly populating the values for a static image.

--- Site/galleries/gallery.js
+++ Site/galleries/gallery.js
@@ -83,6 +83,7 @@ gallery.populateNameAndDescription = function() {
    //
    // For debugging
    //
+   console.log( 'populateNameAndDescription - this.pathToImages: ' + this.pathToImages )
    $('#gallery-to-show').text( this.galleryToShow );
    $('#json-file-name').text( this.jsonFileName );
    $('#path-to-images').text( this.pathToImages );
@@ -92,28 +93,45 @@ gallery.populateNameAndDescription = function() {
  * Callback from getJSON call that processes the JSON we get
  */
 gallery.populateGallery = function( images ) {
+   //
+   // call this "proof of concept"
+   //
+   console.log( 'populateGallery - gallery.pathToImages: ' + gallery.pathToImages )
+   var full_path_to_image;
+   full_path_to_image = gallery.pathToImages + images[0].image_file_name;  // "this." does not work in callbacks
    console.log( 'images.length = ' + images.length );
    $('#number-of-images').text( images.length );
    $('#image-zero-id').text( images[0].id );
    $('#image-zero-image-name').text( images[0].image_name );
-   $('#image-zero-image-file-name').text( images[0].image_file_name );
    $('#image-zero-four-letter-type').text( images[0].four_letter_type );
+   $('#image-zero-image-file-name').text( images[0].image_file_name );
+   $('#image-zero-full-path-to-image').text( full_path_to_image );
+   //
+   // our first image is a static image
+   //
+   full_path_to_image = gallery.pathToImages + images[1].image_file_name;  // "this." does not work in callbacks
+   $('#static-image-img').attr( "src", full_path_to_image );
+   $('#static-image-figcaption').text( images[1].image_name );
+   $('#static-image-frontpage-blurb').text( images[1].frontpage_blurb );
 }
 
 /**
- ************************************
- * Code to call the functions in the proper sequence
- ************************************
- */
-
-/*
+ ********************************************
+ * Code to assemble the data needed and
+ * call the functions in the proper sequence
+ ********************************************
+ *
  * Get the name of the gallery to show from the query variable (in the url)
- * Get the corresponding json file containing the data for the gallery to show
- *   setting a callback function to populate the gallery with each image and its data.
  */
 gallery.galleryToShow = gallery.getQueryVariable( 'gallery' );
 gallery.jsonFileName = 'json/' + gallery.galleryToShow + '.json';
-gallery.pathToImages = '../images/galleries/' + gallery.galleryToShow + '/';
+gallery.pathToImages = '../../images/galleries/' + gallery.galleryToShow + '/';
+
+/*
+ * (1) Fill in the name and description of the gallery.
+ * (2) Get the corresponding json file containing the data for the gallery to show,
+ *     setting a callback function to populate the gallery with each image and its data.
+ */
 gallery.populateNameAndDescription();
 
 $.getJSON( gallery.jsonFileName, gallery.populateGallery );
