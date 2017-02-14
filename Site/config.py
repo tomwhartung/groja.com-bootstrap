dbc240783a869a9b585cf758679dcc6ebe5842fc
Tom W Hartung <tomwhartung@gmail.com>
2017-02-14 13:07:54 -0700

    Adding first pass at config.py .  Changes will be coming (e.g., th the SECRET_KEY) but this does what we want it to for now.

---
+++ Site/config.py
@@ -0,0 +1,29 @@
+##
+# Flask configuration for groja.com
+# ---------------------------------
+# After a bit of research, we are going to use the method presented here:
+#   http://flask.pocoo.org/docs/0.11/config/
+# Another resource:
+#   https://realpython.com/blog/python/flask-by-example-part-1-project-setup/
+#
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config( object ):
+   DEBUG = False
+   TEMPLATES_AUTO_RELOAD = True
+   CSRF_ENABLED = True
+   SECRET_KEY = 'abcdefgHIJKLMNOPqRsTuVwXyZ'
+
+class ProductionConfig( object ):
+   DEBUG = False
+   TESTING = False
+
+class DevelopmentConfig( object ):
+   DEBUG = True
+   TESTING = False
+
+class TestingConfig( object ):
+   DEBUG = True
+   TESTING = True
+
