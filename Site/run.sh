6f2a3977250728cfdd86574fcf7ed4de2b8cebeb
Tom W Hartung <tomwhartung@gmail.com>
2017-02-22 14:03:02 -0700

    Updated run.sh to set the debug flag.

--- Site/run.sh
+++ Site/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
-# Run the flask development server
+#  Run the flask development server
+#  We set the debug flag here so that we don't have to set it in the code and
+#    won't risk having it on in production
+#  Reference: http://flask.pocoo.org/docs/0.12/cli/#debug-flag
 #
+export FLASK_DEBUG=1
 export FLASK_APP=groja.py
 python3 -m flask run
