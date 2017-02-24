85de04fd21b0c02fb2b828333ce03eb6d221ab35
Tom W Hartung <tomwhartung@gmail.com>
2017-02-24 10:46:29 -0700

    Adding an empty (devoid of commands, not comments!) env.sh file, because I am getting used to having one in these Site directories.  Yes it contains comments explaining why it is "empty."

---
+++ Site/env.sh
@@ -0,0 +1,8 @@
+#
+#  This env.sh file is empty!
+#  --------------------------
+#  Live sites run using the global version of installed packages
+#
+#  NOTE:
+#     When developing, run python3 and pip3 instead of python and pip!
+#
