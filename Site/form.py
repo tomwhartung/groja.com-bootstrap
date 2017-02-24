ade53a1efb8e774d83f92243c30614e65a2c708b
Tom W Hartung <tomwhartung@gmail.com>
2017-02-24 10:41:49 -0700

    Adding form.py from the 14-flask_frankenform_with_db/Site project in the always_learning_python repo.

---
+++ Site/form.py
@@ -0,0 +1,23 @@
+##
+#  Define the forms used by this app
+#  For references, see the README.md file:
+#     https://github.com/tomwhartung/always_learning_python/tree/master/13-flask_frankenforms_exp-4
+#
+## from wtforms import Form
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Optional, Required, Email
+
+##
+#  Define a form to get the visitor's name and email address
+#
+class NameEmailForm( Form ):
+   name = StringField( 'Name:', validators=[Optional()] )
+   email = StringField( 'Email:',
+      [Required("Please share your email address so that we can contact you."),
+       Email("Please enter a valid email address.")] )
+   submit = SubmitField( 'Get Your Portrait' )
+
+   def reset(self):
+      blankData = MultiDict([ ('csrf', self.reset_csrf() ) ])
+      self.process(blankData)
