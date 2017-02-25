f578e07b6657ccd2ab3895f47e4b6a1fa8378bf6
Tom W Hartung <tomwhartung@gmail.com>
2017-02-25 14:39:57 -0700

    Adding the thankyou route and function and updating the contactme function with code copied from the 14-flask_frankenform_with_db project in the always_learning_python repo.

--- Site/groja.py
+++ Site/groja.py
@@ -4,7 +4,7 @@
 # Purpose: link routes to their corresponding templates
 # Reference: Chapter 3 of the "Flask Web Development" book
 #
-from flask import Flask, render_template
+from flask import Flask, flash
 from flask_bootstrap import Bootstrap
 from flask import redirect, render_template, request, session, url_for
 from form import NameEmailForm
@@ -54,11 +54,43 @@ def yourportrait() :
    return render_template( 'yourportrait.html', yourportraitSelected='selected' )
 
 ##
-# Show the Books and Sites page:
+# Display contactme page with the form, and process it when it comes back
 #
-@app.route( '/contactme' )
-def contactme() :
-   return render_template( 'contactme.html', contactmeSelected='selected' )
+@app.route("/contactme", methods=['GET', 'POST'])
+def contactme():
+   form = NameEmailForm( request.form )
+
+   if request.method == 'POST':
+      name = form.name.data
+      email = form.email.data
+      print( "name: ", name, "email: ", email )
+
+      if form.validate():
+         session['name'] = name
+         insert_name_email( name, email, portrait=1 )
+         flash( 'Thanks, we will be in touch with you soon!' )
+         return redirect( url_for('thanks') )
+      else:
+         ## print( "form.errors:", form.errors )
+         #
+         #  key = 'email', values = [] (list of error messages)
+         #
+         for key, value in form.errors.items():
+            for message in value:
+               flash( message )
+   else:
+      form.name.data = ''
+      form.email.data = ''
+
+   return render_template( 'contactme.html', form=form )
+
+##
+# Thank the visitor for sharing their email address
+#
+@app.route( "/thanks" )
+def thanks():
+   name = session.get( 'name' )
+   return render_template( 'thanks.html', name=name )
 
 ##
 # Run the app!
