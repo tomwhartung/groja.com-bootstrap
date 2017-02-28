994ed2321dd485a5820e98378b45666c10e82c4b
Tom W Hartung <tomwhartung@gmail.com>
2017-02-28 15:30:55 -0700

    Adding send_email.py , which contains a function to send a notification email when a visitor shares their email address.

---
+++ Site/send_email.py
@@ -0,0 +1,32 @@
+##
+#  Module to send an email notification of interest
+#  Reference:
+#     https://docs.python.org/3/library/email-examples.html
+#
+import smtplib
+from email.mime.text import MIMEText
+
+import os
+GROJA_MAIL_FROM = os.environ.get( 'GROJA_MAIL_FROM' )
+GROJA_MAIL_TO = os.environ.get( 'GROJA_MAIL_TO' )
+## from groja import GROJA_MAIL_FROM, GROJA_MAIL_TO
+
+##
+#  Send an email getting the TO and FROM values from environment variables
+#
+#  Reference:
+#     https://docs.python.org/3/library/email-examples.html
+#
+def send_interest_email( message_text ):
+   ## print( 'In the send_test_email() in groja.py, message_text =', message_text )
+   print( 'GROJA_MAIL_FROM:', GROJA_MAIL_FROM )
+   print( 'GROJA_MAIL_TO:', GROJA_MAIL_TO )
+   msg = MIMEText( message_text )
+   msg['Subject'] = 'Test Email (Subject)'
+   msg['From'] = GROJA_MAIL_FROM
+   msg['To'] = GROJA_MAIL_TO
+   server = smtplib.SMTP('localhost')
+   server.send_message(msg)
+   server.quit()
+   print( 'Message sent!' )
+   return True
