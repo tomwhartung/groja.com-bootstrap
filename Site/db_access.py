c02980ab978dce1c2eba21dcbbc001bfca820b86
Tom W Hartung <tomwhartung@gmail.com>
2017-02-28 15:46:25 -0700

    Updated db_access.py to not allow duplicate emails but rather to update the existing row when an existing email address is submitted.

--- Site/db_access.py
+++ Site/db_access.py
@@ -12,21 +12,93 @@ NAME_EMAIL_TABLE = DB_DIRECTORY + 'NameEmail.db'
 
 ################################################################################
 #
-#  Functions:
-#  ----------
+#  Function definitions:
+#  =====================
+#  Database access functions are most important and so appear first
+#  ----------------------------------------------------------------
+##
+#  Update an old or insert a new row into the table, as appropriate
 #
+def update_or_insert_name_email( name, email, id=0, consulting=-1, newsletter=-1, portrait=-1 ):
+   if id == 0:
+      result = email_already_in_db( email )
+      if result:
+         id = result[0]
+         print( 'Updating the email that is already in the db, id =', id )
+         with sqlite3.connect( NAME_EMAIL_TABLE ) as connection:
+            query = 'UPDATE NameEmail SET name = "' + name + '"'
+            if consulting != -1:
+               query += ', consulting = ' + str(consulting)
+            if newsletter != -1:
+               query += ', newsletter = ' + str(newsletter)
+            if portrait != -1:
+               query += ', portrait = ' + str(portrait)
+            query += ' WHERE id = ' + str(id)
+            curs = connection.cursor()
+            curs.execute( query )
+      else:
+         # default ("-1") means not specified means "0" when inserting
+         if consulting == -1:
+            consulting = 0
+         if newsletter == -1:
+            newsletter = 0
+         if portrait == -1:
+            portrait = 0
+         insert_name_email( name, email, consulting, newsletter, portrait )
+   else:
+      print( 'We got an id passed in, and we are not yet prepared to handle it:', id )
+
+###   with sqlite3.connect( NAME_EMAIL_TABLE ) as connection:
+###      curs = connection.cursor()
+###      curs.execute(
+###         "INSERT INTO NameEmail (name,email,consulting,newsletter,portrait) VALUES (?,?,?,?,?)",
+###            ( name, email, consulting, newsletter, portrait ) )
+
+   return True
+
+##
+#  Check for existing email address
+#  If it's there, return the row, else return False
+#
+def email_already_in_db( email ):
+   result = False
+   with sqlite3.connect( NAME_EMAIL_TABLE ) as connection:
+      curs = connection.cursor()
+      ## query = 'SELECT * from NameEmail WHERE email = "' + email + "'"
+      query = 'SELECT * from NameEmail WHERE email = "' + email + '"'
+      curs.execute( query )
+      row = curs.fetchone()
+      print( 'in email_already_in_db, row:', row )
+      result = row
+   return result
+
 ##
 #  Insert a row into the table
 #
 def insert_name_email( name, email, consulting=0, newsletter=0, portrait=0 ):
    with sqlite3.connect( NAME_EMAIL_TABLE ) as connection:
       curs = connection.cursor()
-      ## cur.execute("INSERT INTO account_holder (email,username,phone,password) VALUES (?,?,?,?)", (email,username,phone,password))
       curs.execute(
          "INSERT INTO NameEmail (name,email,consulting,newsletter,portrait) VALUES (?,?,?,?,?)",
             ( name, email, consulting, newsletter, portrait ) )
    return True
 
+##
+#  Return a string containing the date and the time formatted the same as the
+#  Sqlite3 CURRENT_TIMESTAMP used in the schema, i.e.,
+#     CURRENT_TIMESTAMP returns dates in the format "1970-01-01 00:00:00"
+#
+def my_current_timestamp():
+   my_time = datetime.time(1, 2, 3)
+   my_date = datetime.date.today()
+   my_current_timestamp = datetime.datetime.combine( my_date, my_time )
+   return my_current_timestamp
+
+
+################################################################################
+#
+#  Functions for testing and development, etc.:
+#  --------------------------------------------
 ##
 #  Insert a row of hard-coded values into the table
 #  We wrote this before the above (general) function and are keeping it (for now)
@@ -40,17 +112,6 @@ def insert_hard_coded_name_email():
          "INSERT INTO NameEmail (name,email,date_added,date_changed) VALUES (?,?,?,?)",
             ( 'Sam', 'sam@sam.com', my_current_timestamp(), my_current_timestamp() ) )
    return True
-
-##
-#  Return a string containing the date and the time formatted the same as the
-#  Sqlite3 CURRENT_TIMESTAMP used in the schema, i.e.,
-#     CURRENT_TIMESTAMP returns dates in the format "1970-01-01 00:00:00"
-#
-def my_current_timestamp():
-   my_time = datetime.time(1, 2, 3)
-   my_date = datetime.date.today()
-   my_current_timestamp = datetime.datetime.combine( my_date, my_time )
-   return my_current_timestamp
 
 ##
 #  Print all rows in the table to stdout
@@ -88,7 +149,7 @@ def see_data( rows, line_prefix="" ):
 #     >>> test_insert_functions()
 #  Or, use these commands to test on the command line:
 #     $ . ./env.sh
-#     $ python
+#     $ python3
 #     >>> from db_access import insert_hard_coded_name_email
 #     >>> insert_hard_coded_name_email()
 #     >>> from db_access import insert_name_email
