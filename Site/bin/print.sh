30b5510d843ec058cb5cfdd051def7a3416d13e1
Tom W Hartung <tomwhartung@gmail.com>
2017-03-24 15:21:21 -0600

    Updated  print.sh to work in this new directory, by adding the parent directory to the PYTHONPATH .  Also added another way (less typing) we can run it, but I like the long way, because it should still work even if we change the default functionality of ../db_access.py .

--- Site/bin/print.sh
+++ Site/bin/print.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+#
+# print the contents of our sqlite3 database
+#
+export PYTHONPATH="..:${PYTHONPATH}"
+
+## python3 -m db_access
 python3 -c "from db_access import print_table ; print_table()"
