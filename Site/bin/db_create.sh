42f0c90a046e792dc8c9b10c60463d580ac7a302
Tom W Hartung <tomwhartung@gmail.com>
2017-03-25 13:38:27 -0600

    Updated db_create.sh to run print.sh in this directory, so in the event that we might change the python module, we just have to change the one script and not both.

--- Site/bin/db_create.sh
+++ Site/bin/db_create.sh
@@ -2,7 +2,8 @@
 #
 # Create and print the db
 #
+export PATH=".:${PATH}"
 export PYTHONPATH="..:${PYTHONPATH}"
 
 python3 -m db_create
-python3 -c "from db_access import print_table ; print_table()"
+print.sh
