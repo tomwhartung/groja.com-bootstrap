66241b5dc8294fb9d2ff769d66ca2153c8ea8292
Tom W Hartung <tomwhartung@gmail.com>
2017-03-25 15:10:15 -0600

    Updated run.sh to run in its new directory.

--- Site/bin/run.sh
+++ Site/bin/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
-#  Run the flask development server
-#  We set the debug flag here so that we don't have to set it in the code and
-#    won't risk having it on in production
-#  Reference: http://flask.pocoo.org/docs/0.12/cli/#debug-flag
+# Run the flask development server
+# We set the debug flag here so that we don't have to set it in the code and
+#   won't risk having it on in production
+# Reference: http://flask.pocoo.org/docs/0.12/cli/#debug-flag
 #
+## export PYTHONPATH="..:${PYTHONPATH}"
 export FLASK_DEBUG=1
 export FLASK_APP=groja.py
-python3 -m flask run
+
+cd .. && python3 -m flask run
