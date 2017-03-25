b5b52b68644644182758a1e9ad95eea6f57640e9
Tom W Hartung <tomwhartung@gmail.com>
2017-03-25 13:36:40 -0600

    Updated db_create.sh to work in this new subdirectory.

--- Site/bin/db_create.sh
+++ Site/bin/db_create.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
-#  Create and print the db
+# Create and print the db
 #
+export PYTHONPATH="..:${PYTHONPATH}"
+
 python3 -m db_create
 python3 -c "from db_access import print_table ; print_table()"
