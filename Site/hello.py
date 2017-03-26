3ffb6a6cd9673cd9ade9f930b46cfb6c2b2743b8
Tom W Hartung <tomwhartung@gmail.com>
2017-03-26 11:38:02 -0600

    Updated hello.py to be PEP8 compliant.

--- Site/hello.py
+++ Site/hello.py
@@ -1,25 +1,30 @@
-##
-# From Chapter 3 of the "Flask Web Development" book
-# --------------------------------------------------
-#
-# Experimenting with templates
-#
+""" Ye olde hello world sanity check, using only the base template.
+
+Purpose: ensure we can process the request to render a simple template
+Author: Tom W. Hartung
+Date: Winter, 2017
+Copyright: (c) 2017 Tom W. Hartung, Groja.com, and JooMoo Websites LLC.
+Reference:
+    Chapter 3 of the "Flask Web Development" book (M.Grinberg 2014)
+Usage:
+    bin/hello-run.sh
+"""
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
-app = Flask( __name__ )
-Bootstrap( app )
+app = Flask(__name__)
+Bootstrap(app)
 
-##
-# Use a template to say hello.
+
+@app.route('/')
+def index():
+    """ Say "hello" by rendering the hello.html template """
+    return render_template('hello.html')
+
 #
-@app.route( '/' )
-def index() :
-   return render_template( 'base.html' )
-
-##
 # Run the app!
 #
-if __name__ == '__main__' :
-   app.run( debug=True )
+if __name__ == '__main__':
+    app.run(debug=True)
 
