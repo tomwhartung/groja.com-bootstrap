ccd0cdac28ed26df3296f1431f1f0ea67a4e457d
Tom W Hartung <tomwhartung@gmail.com>
2017-03-26 11:14:15 -0600

    Updated form.py to be PEP8 compliant.

--- Site/form.py
+++ Site/form.py
@@ -1,22 +1,32 @@
-##
-#  Define the forms used by this app
-#  For references, see the README.md file:
-#     https://github.com/tomwhartung/always_learning_python/tree/master/13-flask_frankenforms_exp-4
-#
+""" Define the forms used by this app
+
+Purpose: class that defines the form used on the Contact Me page
+Author: Tom W. Hartung
+Date: Winter, 2017
+Copyright: (c) 2017 Tom W. Hartung, Groja.com, and JooMoo Websites LLC.
+References:
+    See the README.md file
+    https://github.com/tomwhartung/always_learning_python/tree/master/13-flask_frankenforms_exp-4  # noqa
+"""
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Optional, Required, Email
 
-##
-#  Define a form to get the visitor's name and email address
-#
-class NameEmailForm( FlaskForm ):
-   name = StringField( 'Name:', validators=[Optional()] )
-   email = StringField( 'Email:',
-      [Required("Please share your email address so that we can contact you."),
-       Email("Please enter a valid email address.")] )
-   submit = SubmitField( 'Get Your Portrait' )
 
-   def reset(self):
-      blankData = MultiDict([ ('csrf', self.reset_csrf() ) ])
-      self.process(blankData)
+class NameEmailForm(FlaskForm):
+
+    """ Define a form to get the visitor's name and email address """
+
+    name = StringField('Name:', validators=[Optional()])
+    email = StringField(
+            'Email:',
+            [Required("Share your email address so we can contact you."),
+                Email("Please enter a valid email address.")]
+    )
+    submit = SubmitField('Get Your Portrait')
+
+    def reset(self):
+        """ Reset the form """
+        blankData = MultiDict([('csrf', self.reset_csrf())])
+        self.process(blankData)
